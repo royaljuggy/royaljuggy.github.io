21164c0657b658ca52d3c22a4f7341dc6837b3b0
royaljuggy <dragonte@hotmail.ca>
2026-02-07 13:37:14 -0500

    fix: unregister service workers and disable aggressive caching to fix white screen issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+// Unregister any old service workers from previous site versions
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <HashRouter>
     <App />
